4fa2697bbb8e7914f191b0425b1680832111bdb7
Sebastian Matyszewski <sebastian@Sebastians-iMac.local>
2020-06-01 13:23:39 +0200

    Initial Commit

---
+++ BattleShip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BattleShip
+//
+//  Created by Sebastian Matyszewski on 01/06/2020.
+//  Copyright © 2020 Sebastian Matyszewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
