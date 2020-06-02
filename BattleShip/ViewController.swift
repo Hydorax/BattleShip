fa5c64e5a11e47deaf80e1f20fb44b578cde2968
Sebastian Matyszewski <sebastian@Sebastians-iMac.local>
2020-06-02 15:00:45 +0200

    mesracze

--- BattleShip/ViewController.swift
+++ BattleShip/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
